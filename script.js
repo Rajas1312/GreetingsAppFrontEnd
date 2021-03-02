fbfc813ea82e0ba4d2d5c329624b8020dbffc6cb
Rajas1312 <you@example.com>
2021-03-02 22:37:56 +0530

    refactor : for loop to add greetings in script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 fetch('http://localhost:8000/greeting/')
-    .then(res => res.json()).then(data => console.log(data))
+    .then(res => res.json()).then(data => console.log(data)
+    )
 
 fetch('http://localhost:8000/greeting/', {
     method: 'POST',
@@ -28,3 +29,27 @@ fetch('http://localhost:8000/greeting/603e1ef082ef1f1c3c979f50', {
 })
     .then(res => res.json())
     .then(res => console.log(res))
+
+for (let i = 0; i < 7; i++) {
+    var timeDiv = `
+            <div>
+                <span>ObjectId("24113cd12w1")</span>
+            </div>
+            <div>
+                <h4>Hello</h4>
+                <span>(greetings)</span>
+
+            </div>
+            <div>
+                <h4>Rajas</h4>
+                <span>(name)</span>
+            </div>
+            <div>
+                <span>15 min ago</span>
+            </div>
+        `
+    var div = document.createElement("div");
+    div.className = ('greetings');
+    div.innerHTML = timeDiv;
+    document.querySelector('.greetingsContainer').appendChild(div);
+}
