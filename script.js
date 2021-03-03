bb114efffc1cff58a7df15982723f81f331bbe51
Rajas1312 <you@example.com>
2021-03-04 00:07:40 +0530

    feat(post) : implement add feature in greetings

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ fetch('http://localhost:8000/greeting/')
     .then(res =>
         res.json()
     ).then(data => {
-        console.log(data)
         for (let i = 0; i < data.result.length; i++) {
             var timeDiv = `
-                      
+                      <button onclick="edit('${data.result[i]._id}')">Edit</button>
                         <div>
-                            <h4>Hello</h4>
+                            <h4>${data.result[i].greeting}</h4>
                             <span>(greetings)</span>
 
                         </div>
@@ -26,16 +25,37 @@ fetch('http://localhost:8000/greeting/')
         }
     })
 
-
-fetch('http://localhost:8000/greeting/', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    }, body: JSON.stringify({
-        name: "Misbah",
-        greeting: "Hey"
+function edit(b) {
+    fetch('http://localhost:8000/greeting/' + b, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({
+            name: document.querySelector('#fname').value,
+            greeting: document.querySelector('#lname').value
+        })
     })
-})
+        .then(res => res.json())
+        .then(res => console.log(res))
+}
+
+function create() {
+    fetch('http://localhost:8000/greeting/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({
+            name: document.querySelector('#fname').value,
+            greeting: document.querySelector('#lname').value
+        })
+    })
+
+    document.querySelector('.greetings-form').style.display = 'none';
+}
+function display() {
+    document.querySelector('.greetings-form').style.display = 'block';
+}
+
 
 // fetch('http://localhost:8000/greeting/603e3c115e318d04e45caa8b', {
 //     method: 'DELETE',
@@ -43,15 +63,6 @@ fetch('http://localhost:8000/greeting/', {
 //     .then(res => res.json())
 //     .then(res => console.log(res))
 
-// fetch('http://localhost:8000/greeting/603e1ef082ef1f1c3c979f50', {
-//     method: 'PUT',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     }, body: JSON.stringify({
-//         name: "Shivam",
-//         greeting: "Hello"
-//     })
-// })
-//     .then(res => res.json())
-//     .then(res => console.log(res))
+
+
 
