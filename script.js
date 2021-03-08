491d6fc087359bda3da0f7fc60d74d62d3a4d25c
Rajas1312 <you@example.com>
2021-03-08 08:46:10 +0530

    feat : add modal to add edit and delete

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let glo = 0;
+let flag = 0;
 let names, greeting;
-
 function find() {
     fetch('http://localhost:8000/greeting/')
         .then(res =>
             res.json()
         ).then(data => {
-            for (let greet = 0; greet < data.result.length; greet++) {
-                var timeDiv = `
+            for (let greet = data.result.length - 1; greet >= 0; greet--) {
+                let timeDiv = `
                      
                         <div>
                             <h4>${data.result[greet].greeting}</h4>
@@ -21,20 +20,23 @@ function find() {
                         <div class="silvasa">
                             <span>15 min ago</span>
                             <button onclick="edit('${data.result[greet]._id}','${data.result[greet].greeting}','${data.result[greet].name}')"><i class="fa fa-edit " ></i></,onclick=>
-                            <button onclick="remove('${data.result[greet]._id}')"<i class="fa fa-trash " ></i></button>
+                            <button onclick="remove1('${data.result[greet]._id}')"<i class="fa fa-trash " ></i></button>
                         </div>
                     `
-                var div = document.createElement("div");
+                let div = document.createElement("div");
                 div.className = ('greetings');
                 div.innerHTML = timeDiv;
                 document.querySelector('.greetingsContainer').appendChild(div);
             }
+        }).catch(error => {
+            console.log("error occured while serching the greetings", error)
         })
 }
 find()
 
 function create() {
-    if (glo == 0) {
+
+    if (flag == 0) {
         fetch('http://localhost:8000/greeting/', {
             method: 'POST',
             headers: {
@@ -44,8 +46,7 @@ function create() {
                 greeting: document.querySelector('#lname').value
             })
         })
-
-        document.querySelector('.greetings-form').style.display = 'none';
+        document.querySelector('.greetings-form').style.display = 'flex';
     } else {
         fetch('http://localhost:8000/greeting/' + document.querySelector('#id_store').value + '/', {
             method: 'PUT',
@@ -59,36 +60,49 @@ function create() {
             .then(res => res.json())
             .then(res => {
                 console.log(res);
-                alert('kooko');
-                glo = 0;
+                flag = 0;
                 document.querySelector('.greetingsContainer').style.display = 'flex'
+            }).catch(error => {
+                console.log("error occured while creting greetings", error)
             });
 
     }
 
 }
 function display() {
+    document.getElementById('id01').style.display = 'block'
     document.querySelector('.greetings-form').style.display = 'flex';
-    document.querySelector('.greetingsContainer').style.display = "none";
+    document.querySelector('.greetingsContainer').style.display = "flex";
 }
 
-function remove(b) {
-    fetch('http://localhost:8000/greeting/' + b, {
+function remove1(deleteId) {
+
+    document.querySelector('#id_del').value = deleteId;
+    document.getElementById('id02').style.display = 'block';
+}
+
+function remove() {
+    fetch('http://localhost:8000/greeting/' + document.querySelector('#id_del').value, {
         method: 'DELETE',
     })
         .then(res => res.json())
         .then(res => console.log(res))
+        .catch(error => {
+            console.log("error occoured while deleting greeting", error)
+        })
 }
 
 function edit(b, c, d) {
-    console.log((b));
+    document.getElementById('id01').style.display = 'block'
     document.querySelector('#fname').value = d;
     document.querySelector('#lname').value = c;
-    document.querySelector('.greetingsContainer').style.display = "none";
-    glo = 1;
-    document.querySelector('.greetings-form').style.display = 'flex';
+    //document.querySelector('.greetingsContainer').style.display = "none";
+    flag = 1;
+    //document.querySelector('.greetings-form').style.display = 'flex';
     document.querySelector('#id_store').value = b;
 }
+
+
 
 
 
