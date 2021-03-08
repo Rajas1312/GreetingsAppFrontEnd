dd6eac227e84506f1c5be29c66f9110ef9145a03
Rajas1312 <you@example.com>
2021-03-08 18:20:22 +0530

    feat : add timestamps

--- script.js
+++ script.js
@@ -9,15 +9,12 @@ function findAllGreetings() {
                 let timeDiv = `
                         <div>
                             <h4>${data.result[greet].greeting}</h4>
-                            <span>(greetings)</span>
-
                         </div>
                         <div>
                             <h4>${data.result[greet].name}</h4>
-                            <span>(name)</span>
                         </div>
-                        <div class="silvasa">
-                            <span>15 min ago</span>
+                        <div class="time-edit-button">
+                            <span>${data.result[greet].updatedAt}</span></span>
                             <button onclick="editGreetings('${data.result[greet]._id}','${data.result[greet].greeting}','${data.result[greet].name}')"><i class="fa fa-edit " ></i></onclick=>
                             <button onclick="removeById('${data.result[greet]._id}')"<i class="fa fa-trash " ></i></button>
                         </div>
@@ -26,6 +23,7 @@ function findAllGreetings() {
                 div.className = ('greetings');
                 div.innerHTML = timeDiv;
                 document.querySelector('.greetingsContainer').appendChild(div);
+                console.log(data)
             }
         }).catch(error => {
             console.log("error occured while serching the greetings", error)
@@ -34,7 +32,6 @@ function findAllGreetings() {
 findAllGreetings()
 
 function create() {
-
     fetch(URL, {
         method: 'POST',
         headers: {
@@ -46,56 +43,8 @@ function create() {
     })
     document.querySelector('.greetings-form').style.display = 'flex';
 }
-// function editGreetings1() {
-//     fetch('http://localhost:8000/greeting/' + document.querySelector('#id_store').value + '/', {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }, body: JSON.stringify({
-//             name: document.querySelector('#fname').value,
-//             greeting: document.querySelector('#lname').value
-//         })
-//     })
-//         .then(res => res.json())
-//         .then(res => {
-//             console.log(res);
-//             //flag = 0;
-//             document.querySelector('.greetingsContainer').style.display = 'flex'
-//         }).catch(error => {
-//             console.log("error occured while creting greetings", error)
-//         });
-// }
 
-function display() {
-    document.getElementById('id01').style.display = 'block'
-    document.querySelector('.greetings-form').style.display = 'flex';
-    document.querySelector('.greetingsContainer').style.display = "flex";
-}
-
-function removeById(deleteId) {
-
-    document.querySelector('#id_del').value = deleteId;
-    document.getElementById('id02').style.display = 'block';
-}
-
-function removeGreetings() {
-    fetch(URL + document.querySelector('#id_del').value, {
-        method: 'DELETE',
-    })
-        .then(res => res.json())
-        .then(res => console.log(res))
-        .catch(error => {
-            console.log("error occoured while deleting greeting", error)
-        })
-}
-
-function editGreetings(id, greeting, name) {
-    document.getElementById('id01').style.display = 'block'
-    document.querySelector('#fname').value = name;
-    document.querySelector('#lname').value = greeting;
-    //flag = 1;
-    document.querySelector('#id_store').value = id;
-
+function editGreetings1() {
     fetch('http://localhost:8000/greeting/' + document.querySelector('#id_store').value + '/', {
         method: 'PUT',
         headers: {
@@ -113,6 +62,43 @@ function editGreetings(id, greeting, name) {
         }).catch(error => {
             console.log("error occured while creting greetings", error)
         });
+}
+
+function display() {
+    document.getElementById('add-greetingId').style.display = 'block'
+    document.querySelector('.greetings-form').style.display = 'flex';
+    document.querySelector('.greetingsContainer').style.display = "flex";
+}
+
+function removeById(deleteId) {
+
+    document.querySelector('#id_del').value = deleteId;
+    document.getElementById('delete-greetingId').style.display = 'block';
+}
+
+function remove() {
+    fetch('http://localhost:8000/greeting/' + document.querySelector('#id_del').value, {
+        method: 'DELETE',
+    })
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(error => {
+            console.log("error occoured while deleting greeting", error)
+        })
+}
+
+
+function editGreetings(id, greeting, name) {
+    document.getElementById('add-greetingId').style.display = 'block'
+    document.querySelector('#fname').value = name;
+    document.querySelector('#lname').value = greeting;
+    //flag = 1;
+    document.querySelector('#id_store').value = id;
+}
+
+function regexValidation() {
+    let pattern = /[A-Z]{1}[a-z]{2,}/g
+    return pattern;
 }
 
 
