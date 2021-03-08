7190226f4e5753a322340bb46e251623f86e017e
Rajas1312 <you@example.com>
2021-03-08 15:25:27 +0530

    refactor : css style for form

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let flag = 0;
-let names, greeting;
-function find() {
-    fetch('http://localhost:8000/greeting/')
+const URL = 'http://localhost:8000/greeting/'
+function findAllGreetings() {
+    fetch(URL)
         .then(res =>
             res.json()
         ).then(data => {
             for (let greet = data.result.length - 1; greet >= 0; greet--) {
                 let timeDiv = `
-                     
                         <div>
                             <h4>${data.result[greet].greeting}</h4>
                             <span>(greetings)</span>
@@ -19,8 +18,8 @@ function find() {
                         </div>
                         <div class="silvasa">
                             <span>15 min ago</span>
-                            <button onclick="edit('${data.result[greet]._id}','${data.result[greet].greeting}','${data.result[greet].name}')"><i class="fa fa-edit " ></i></,onclick=>
-                            <button onclick="remove1('${data.result[greet]._id}')"<i class="fa fa-trash " ></i></button>
+                            <button onclick="editGreetings('${data.result[greet]._id}','${data.result[greet].greeting}','${data.result[greet].name}')"><i class="fa fa-edit " ></i></onclick=>
+                            <button onclick="removeById('${data.result[greet]._id}')"<i class="fa fa-trash " ></i></button>
                         </div>
                     `
                 let div = document.createElement("div");
@@ -32,57 +31,55 @@ function find() {
             console.log("error occured while serching the greetings", error)
         })
 }
-find()
+findAllGreetings()
 
 function create() {
 
-    if (flag == 0) {
-        fetch('http://localhost:8000/greeting/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }, body: JSON.stringify({
-                name: document.querySelector('#fname').value,
-                greeting: document.querySelector('#lname').value
-            })
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({
+            name: document.querySelector('#fname').value,
+            greeting: document.querySelector('#lname').value
         })
-        document.querySelector('.greetings-form').style.display = 'flex';
-    } else {
-        fetch('http://localhost:8000/greeting/' + document.querySelector('#id_store').value + '/', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            }, body: JSON.stringify({
-                name: document.querySelector('#fname').value,
-                greeting: document.querySelector('#lname').value
-            })
-        })
-            .then(res => res.json())
-            .then(res => {
-                console.log(res);
-                flag = 0;
-                document.querySelector('.greetingsContainer').style.display = 'flex'
-            }).catch(error => {
-                console.log("error occured while creting greetings", error)
-            });
-
-    }
-
+    })
+    document.querySelector('.greetings-form').style.display = 'flex';
 }
+// function editGreetings1() {
+//     fetch('http://localhost:8000/greeting/' + document.querySelector('#id_store').value + '/', {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }, body: JSON.stringify({
+//             name: document.querySelector('#fname').value,
+//             greeting: document.querySelector('#lname').value
+//         })
+//     })
+//         .then(res => res.json())
+//         .then(res => {
+//             console.log(res);
+//             //flag = 0;
+//             document.querySelector('.greetingsContainer').style.display = 'flex'
+//         }).catch(error => {
+//             console.log("error occured while creting greetings", error)
+//         });
+// }
+
 function display() {
     document.getElementById('id01').style.display = 'block'
     document.querySelector('.greetings-form').style.display = 'flex';
     document.querySelector('.greetingsContainer').style.display = "flex";
 }
 
-function remove1(deleteId) {
+function removeById(deleteId) {
 
     document.querySelector('#id_del').value = deleteId;
     document.getElementById('id02').style.display = 'block';
 }
 
-function remove() {
-    fetch('http://localhost:8000/greeting/' + document.querySelector('#id_del').value, {
+function removeGreetings() {
+    fetch(URL + document.querySelector('#id_del').value, {
         method: 'DELETE',
     })
         .then(res => res.json())
@@ -92,14 +89,30 @@ function remove() {
         })
 }
 
-function edit(b, c, d) {
+function editGreetings(id, greeting, name) {
     document.getElementById('id01').style.display = 'block'
-    document.querySelector('#fname').value = d;
-    document.querySelector('#lname').value = c;
-    //document.querySelector('.greetingsContainer').style.display = "none";
-    flag = 1;
-    //document.querySelector('.greetings-form').style.display = 'flex';
-    document.querySelector('#id_store').value = b;
+    document.querySelector('#fname').value = name;
+    document.querySelector('#lname').value = greeting;
+    //flag = 1;
+    document.querySelector('#id_store').value = id;
+
+    fetch('http://localhost:8000/greeting/' + document.querySelector('#id_store').value + '/', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({
+            name: document.querySelector('#fname').value,
+            greeting: document.querySelector('#lname').value
+        })
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            //flag = 0;
+            document.querySelector('.greetingsContainer').style.display = 'flex'
+        }).catch(error => {
+            console.log("error occured while creting greetings", error)
+        });
 }
 
 
