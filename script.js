d54e219529912b60098cf49648e14f4c0ff4f175
Rajas1312 <you@example.com>
2021-03-09 15:14:16 +0530

    refactor : add time of update in greetings

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let flag = 0;
 const URL = 'http://localhost:8000/greeting/'
-function findAllGreetings() {
+
+findAllGreetings = () => {
     fetch(URL)
         .then(res =>
             res.json()
         ).then(data => {
             for (let greet = data.result.length - 1; greet >= 0; greet--) {
+                let d = new Date(data.result[greet].updatedAt)
                 let timeDiv = `
                         <div>
                             <h4>${data.result[greet].greeting}</h4>
@@ -13,25 +14,25 @@ function findAllGreetings() {
                         <div>
                             <h4>${data.result[greet].name}</h4>
                         </div>
-                        <div class="time-edit-button">
-                            <span>${data.result[greet].updatedAt}</span></span>
-                            <div><a id="edit-greetings" onclick="editGreetings('${data.result[greet]._id}','${data.result[greet].greeting}','${data.result[greet].name}')"><img src="assets/edit_icon3.png" style="height:20px;></a></div>
-                           <div> <a id="delete-greetings" onclick="removeById('${data.result[greet]._id}')"><img src="assets/delete_icon3.png" style="height:20px"></a></div> 
+                            <div class="time">${d.toLocaleTimeString()}</div>
                         </div>
+                            <div class="editflex">
+                            <div><a id="edit-greetings" ><img onclick="editGreetings('${data.result[greet]._id}','${data.result[greet].greeting}','${data.result[greet].name}')" src="assets/edit_icon3.png" style="height:20px;></a></div>
+                           <div><a id="delete-greetings" ><img onclick="removeById('${data.result[greet]._id}')" src="assets/delete_icon3.png" style="height:20px"></a></div> 
+                            </div>
                     `
                 let div = document.createElement("div");
                 div.className = ('greetings');
                 div.innerHTML = timeDiv;
-                document.querySelector('.greetingsContainer').appendChild(div);
-                console.log(data)
+                document.querySelector('.greetingsContainer').appendChild(div)
             }
         }).catch(error => {
-            console.log("error occured while serching the greetings", error)
+            return error;
         })
 }
 findAllGreetings()
 
-function create() {
+create = () => {
     fetch(URL, {
         method: 'POST',
         headers: {
@@ -44,62 +45,56 @@ function create() {
     document.querySelector('.greetings-form').style.display = 'flex';
 }
 
-function editGreetings1() {
-    fetch('http://localhost:8000/greeting/' + document.querySelector('#id_store').value + '/', {
+updateGreetings = () => {
+    fetch(URL + document.querySelector('#id_edit_store').value + '/', {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         }, body: JSON.stringify({
-            name: document.querySelector('#fname').value,
-            greeting: document.querySelector('#lname').value
+            name: document.querySelector('#edit-fname').value,
+            greeting: document.querySelector('#edit-lname').value
         })
     })
         .then(res => res.json())
         .then(res => {
-            console.log(res);
-            //flag = 0;
             document.querySelector('.greetingsContainer').style.display = 'flex'
+            return res
         }).catch(error => {
-            console.log("error occured while creting greetings", error)
+            return error;
         });
 }
 
-function display() {
+display = () => {
     document.getElementById('add-greetingId').style.display = 'block'
     document.querySelector('.greetings-form').style.display = 'flex';
     document.querySelector('.greetingsContainer').style.display = "flex";
 }
 
-function removeById(deleteId) {
-
+removeById = (deleteId) => {
     document.querySelector('#id_del').value = deleteId;
     document.getElementById('delete-greetingId').style.display = 'block';
+    document.getElementById('edit-greetingId').style.display = 'none'
 }
 
-function remove() {
-    fetch('http://localhost:8000/greeting/' + document.querySelector('#id_del').value, {
+remove = () => {
+    fetch(URL + document.querySelector('#id_del').value, {
         method: 'DELETE',
     })
         .then(res => res.json())
         .then(res => console.log(res))
         .catch(error => {
-            console.log("error occoured while deleting greeting", error)
+            return error;
         })
 }
 
-
-function editGreetings(id, greeting, name) {
-    document.getElementById('add-greetingId').style.display = 'block'
-    document.querySelector('#fname').value = name;
-    document.querySelector('#lname').value = greeting;
-    //flag = 1;
-    document.querySelector('#id_store').value = id;
+editGreetings = (id, greeting, name) => {
+    document.getElementById('edit-greetingId').style.display = 'block'
+    document.getElementById('delete-greetingId').style.display = 'none';
+    document.querySelector('#edit-fname').value = name;
+    document.querySelector('#edit-lname').value = greeting;
+    document.querySelector('#id_edit_store').value = id;
 }
 
-function regexValidation() {
-    let pattern = /[A-Z]{1}[a-z]{2,}/g
-    return pattern;
-}
 
 
 
